aeb40253d2d74def07808600a3bafa5a0253c16b
Vitalii Hrytsenko <vitalik.gricenko77@gmail.com>
2025-11-21 03:12:00 +0200

    fix: fix for prod #2

--- frontend/src/components/Cards/ServiceCard.jsx
+++ frontend/src/components/Cards/ServiceCard.jsx
@@ -2,12 +2,13 @@ import { API_URL } from "../../config/api.js";
 
 function ServiceCard({cardClass, card}) {
 
-  console.log(card);
-  
   return (
     <a href={`/services/${card.id}`} className={`service-card ${cardClass}`}>
       <div className="service-card__image">
-        <img src={card?.imageUrl} alt={card.title} />
+        <img src={card?.imageUrl.startsWith("http")
+                        ? card.imageUrl
+                        : `${import.meta.env.VITE_API_URL || "http://localhost:3001"}${card.imageUrl}`
+                      } alt={card.title} />
       </div>
 
       <div className="service-card__body">
